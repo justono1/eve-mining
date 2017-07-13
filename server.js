c8056523b6f4bff4d028354b488b763679ef24fa
Justin Gauthier <justingauthier@Justins-MacBook-Pro.local>
2017-07-13 08:03:39 -0400

    Testing sso

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var cookieParser = require('cookie-parser');
 var {SingleSignOn} = require('eve-singlesignon');
 
 //https://esi.tech.ccp.is/latest/characters/893818612/skillqueue/?datasource=tranquility&token=fUZDWxGo3XWa3sVafYJXEikzuSPlI02byLAYRPxSITe0yZuvfu2XnwrKYfju-a5l_KHKieVhB611jqbHY6niZQ2'
@@ -19,40 +20,57 @@ const sso = new SingleSignOn(CLIENT_ID, SECRET_KEY, CALLBACK_URL);
 var app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cookieParser());
+
 app.get('/login', function(req, res) {
   return res.redirect(sso.getRedirectUrl(null, SCOPES));
 });
 
 app.get('/auth_callback', function(req, res) {
-    // Get an access token for this authorization code
-    sso.getAccessToken(req.query.code).then(result => {
-        localStorage.setItem('access', JSON.stringify(result));
-        // The result contains the access token and expiry time
-        console.log('Access Token:', result.access_token);
-        console.log('Refresh Token:', result.refresh_token);
-        console.log('Expires in:', result.expires_in);
-        // Store the access token so you can use it later
+    var access_token_cookie = req.cookies.access_token;
+    var refresh_token_cookie = req.cookies.refresh_token;
+    if(access_token_cookie === undefined) {
+      sso.getAccessToken(req.query.code).then(result => {
+        res.cookie('access_token', result.access_token, {
+          maxAge: result.expires_in
+        });
 
-        // Access basic character info
-        return sso.verifyAccessToken(result.access_token);
-    })
-    .then(result => {
-      var charInfo = [result.CharacterID, result.CharacterName];
-      localStorage.setItem('charInfo', JSON.stringify(charInfo));
-        // We now have some basic info...
-        console.log('Character ID:', result.CharacterID);
-        console.log('Character Name:', result.CharacterName);
+        if(refresh_token_cookie === undefined) {
+          res.cookie('refresh_token', result.refresh_token, {
+            maxAge: new Date(Date.now() + 900000)
+          });
+        }
 
-        return res.redirect('/');
-    }, err => {
-        // An error occurred
-    });
+        res.redirect('/');
+          // The result contains the access token and expiry time
+          // console.log('Access Token:', result.access_token);
+          // console.log('Refresh Token:', result.refresh_token);
+          // console.log('Expires in:', result.expires_in);
+          // Store the access token so you can use it later
+
+          // Access basic character info
+          // return sso.verifyAccessToken(result.access_token);
+      }).catch((err) => {
+        console.log('There was an error');
+      });
+
+    }
 
 });
 
+// .then(result => {
+//   var charInfo = [result.CharacterID, result.CharacterName];
+//   localStorage.setItem('charInfo', JSON.stringify(charInfo));
+//     // We now have some basic info...
+//     console.log('Character ID:', result.CharacterID);
+//     console.log('Character Name:', result.CharacterName);
+// }, err => {
+//     // An error occurred
+// });
+
 app.use(function(req, res, next) {
   if(req.headers['x-forwarded-proto'] === 'https') {
-    res.redirect('http://' + req.hostname + req.url);
+    res.redirect('https://' + req.hostname + req.url);
   } else {
     next();
   }
