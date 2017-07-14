ce96fc9d04bdf1c69d32d868964884bc7d05a197
Justin Gauthier <justingauthier@Justins-MacBook-Pro.local>
2017-07-13 20:12:56 -0400

    Updating ESI API component

--- app/api/ESI.jsx
+++ app/api/ESI.jsx
@@ -1,11 +1,13 @@
 var axios = require('axios');
-// var cookie = require('react-cookies');
+var cookie = require('react-cookies');
 
-// var access_token = cookie.load('access_token');
+var BASE_URL_PREFIX = 'https://esi.tech.ccp.is/latest';
+var BASE_URL_PREDICATE = '?datasource=tranquility&language=en-us';
+var ACCESS_TOKEN = cookie.load('access_token');
 
 module.exports = {
   getFleetMembers: function(fleetID) {
-    var baseRequest = `/fleets/${fleetID}/members/${BASE_URL_PREDICATE}&token=${access_token}`;
+    var baseRequest = `/fleets/${fleetID}/members/${BASE_URL_PREDICATE}&token=${ACCESS_TOKEN}`;
     var requestUrl = `${BASE_URL_PREFIX}${baseRequest}`;
     return axios.get(requestUrl).then((data) => {
       console.log(data);
